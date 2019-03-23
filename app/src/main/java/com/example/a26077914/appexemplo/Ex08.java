58f01af26f7cd951566c2dd64456c83382bd17df
26077914 <emersonrolim@gmail.com>
2019-03-22 21:28:05 -0300

    Exercicio 08

---
+++ app/src/main/java/com/example/a26077914/appexemplo/Ex08.java
@@ -0,0 +1,18 @@
+package com.example.a26077914.appexemplo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Ex08 extends AppCompatActivity {
+    private String mdveiculo;
+    private Double ptmotor;
+    private Double distancia;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ex08);
+    }
+}
---
+++ app/src/main/java/com/example/a26077914/appexemplo/Ex08Result.java
@@ -0,0 +1,13 @@
+package com.example.a26077914.appexemplo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Ex08Result extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ex08_result);
+    }
+}
